7a2825c3df735836236265a4583551c4bf252d69
savin ilya <savin15ilya3112@gmail.com>
2025-11-17 09:12:41 +0700

    created a user model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class AdvUser(AbstractUser):
+
+email = models.EmailField(unique=True, null=False, verbose_name='Email адрес')
+patronymic = models.CharField(max_length=200)
+
+def __str__(self):
+    return self.get_full_name()
+
+class Meta:
+    permissions = (("moderator_access", "Доступ модератора"),)
--- designpro/settings.py
+++ designpro/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tomsk'
 
 USE_I18N = True
 
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'catalog.AdvUser'
