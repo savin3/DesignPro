0764a5fd635b08415687ff55c658be919e13caae
savin ilya <savin15ilya3112@gmail.com>
2025-11-19 08:59:03 +0700

    fix: redirection to the main page has been changed

--- designpro/urls.py
+++ designpro/urls.py
@@ -18,11 +18,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.views.generic import RedirectView
+
 from designpro import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls'))
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=False)),
 ]
 
 if settings.DEBUG:
